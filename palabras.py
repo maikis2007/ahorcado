836a0c719f01f4880ff12625974ff28ef0ec3d45
Michael Cabello <maikitoprogramando@gmail.com>
2025-03-04 16:12:49 -0500

    Quitando y agregando

---
+++ palabras.py
@@ -0,0 +1,51 @@
+palabras_faciles = (
+    "gato", "mesa", "sol", "casa", "rojo", "aire", "ojo", "piel", "mar", "pez",
+    "pan", "luz", "sal", "té", "voz", "río", "flor", "luna", "oro", "agua",
+    "miel", "pelo", "mano", "pie", "rey", "fe", "paz", "fin", "mal", "bien",
+    "caña", "león", "ave", "sed", "tren", "cine", "kilo", "isla", "eco", "rama",
+    "nido", "árbol", "éxito", "ícono", "útil", "alma", "vida", "cuna", "sur", "ola",
+    "dos", "tres", "uno", "red", "tez", "dar", "ver", "ser", "duro", "suave",
+    "frío", "calor", "alto", "bajo", "feo", "guapo", "sano", "malo", "lento", "joven",
+    "viejo", "nuevo", "salvo", "humo", "sola", "vaca", "lupa", "cima", "nube", "hola",
+    "masa", "rata", "cola", "seda", "gota", "nudo", "vela", "palo", "bola", "rosa",
+    "coro", "loro", "lago", "faro", "pino", "toro", "leche", "cero", "copa", "piso",
+    "hilo", "dado", "ruta", "muro", "duda", "zeta", "xilófono", "rueda", "fina", "goma",
+    "lana", "mira", "bota", "lima", "risa", "cama", "polo", "liso", "nene", "moto",
+    "bebe", "rana", "bota", "rosa", "pico", "vale"
+)
+
+palabras_intermedias = (
+    "ventana", "guitarra", "planeta", "montaña", "pingüino", "cielo", "estrella", "bosque", "prado", "libro",
+    "cuadro", "sombra", "brisa", "nube", "arena", "rayo", "fuego", "llama", "corona", "roble",
+    "oliva", "piedra", "lago", "río", "selva", "mueble", "silla", "carta", "banco", "chico",
+    "mujer", "niño", "razón", "magia", "guerra", "paz", "calor", "frío", "sueño", "azul",
+    "verde", "blanco", "negro", "plata", "oro", "bronce", "acero", "madera", "hierro", "pasto",
+    "rocío", "lluvia", "nieve", "viento", "sabor", "dulce", "amargo", "salado", "miel", "crema",
+    "vino", "cerveza", "fruta", "manzana", "pera", "uva", "mango", "papaya", "cereza", "melón",
+    "plátano", "limón", "naranja", "pomelo", "fresa", "kiwi", "albaricoque", "ciruela", "durazno", "tuna",
+    "anís", "canela", "clavo", "menta", "tomate", "zanahoria", "lechuga", "apio", "brócoli", "coliflor",
+    "calabaza", "pepino", "remolacha", "chayote", "ejote", "haba", "lenteja", "frijol", "arroz", "pasta",
+    "pollo", "carne", "pescado", "huevo", "queso", "panela", "mantequilla", "yogur", "crema", "salsa",
+    "aceite", "vinagre"
+)
+
+palabras_dificiles = (
+    "arquitectura", "hipopótamo", "murciélago", "extraordinario", "jurisprudencia", "universidad", "complejidad",
+    "inconstitucional", "desafiante", "controversia", "responsabilidad", "incomparabilidad", "filosofía", "psicología",
+    "desarrollador", "programación", "computación", "inteligencia", "innovación", "creatividad", "transformación",
+    "dinamismo", "productividad", "comunicación", "globalización", "revolucionario", "sostenibilidad", "autenticidad",
+    "influencia", "perspectiva", "transparencia", "eficiencia", "integridad", "colaboración", "optimización",
+    "vulnerabilidad", "sustentabilidad", "responsable", "competitividad", "experimentación", "conmemoración",
+    "constitucional", "influenciable", "inconcebible", "desestabilizar", "desproporcionado", "intransigencia",
+    "irresponsabilidad", "multicultural", "descentralizar", "estadística", "complementario", "metamorfosis",
+    "desempeñarse", "subjetividad", "objetividad", "perseverancia", "resiliencia", "reconocimiento", "autodidacta",
+    "impredecible", "inconcebible", "magnánimo", "inmortalidad", "excepcional", "inaccesible", "extraordinario",
+    "impecabilidad", "interdisciplinar", "hiperactivo", "desenfrenado", "inmanejable", "irrefutable", "innegable",
+    "manifestación", "desproporcionado", "incompatibilidad", "desintegración", "insostenibilidad", "contradictorio",
+    "cuestionamiento", "revolucionario", "insuperable", "autonomía", "subdesarrollado", "hiperventilación",
+    "transcendental", "inconcebible", "imprevisible", "irresistible", "pseudociencia", "anticonstitucional",
+    "desproporcionado", "antidiscriminatorio", "desafortunadamente", "inconstitucionalidad", "antropomorfismo",
+    "paralelepípedo", "desoxirribonucleico", "otorrinolaringólogo", "esternocleidomastoideo", "electroencefalograma",
+    "anticonceptivo", "desoxirribonucleico", "ininteligible", "antipatriótico", "anticonvulsivo", "hipercolesterolemia",
+    "desoxirribonucleico", "incomprensible", "desoxirribonucleico", "otorrinolaringología"
+)
